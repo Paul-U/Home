9b732098d8e6ab0a86efc7a1a68fcdb9c6e1ea31
Paul-U <kouzukekouryu@gmail.com>
2014-02-09 11:50:11 +0900

    初期化スクリプトディレクトリを読み込む設定を追加。

--- init.py
+++ init.py
@@ -5,20 +5,19 @@ import re;
 
 foo=lambda s:os.path.abspath(os.path.normpath(s))
 
-fnames = ([(root[2:],fname)
-          for root,dirs,fnames in os.walk(".")
-              if not ".git" in root
-          for fname in fnames
-              if not ".git" in fname]);
+fnames = [(root[2:],fname)
+        for root,dirs,fnames in os.walk(".") if not ".git" in root and "./init.d" != root
+        for fname in fnames if not ".git" in fname
+        ]
 ignore= ("./README.org", "./init.py", "./add-dotfiles-repos");
 fnames = ([item for item in fnames
-           if not foo(os.path.join(item[0],item[1])) in [foo(file) for file in ignore]]);
+    if not foo(os.path.join(item[0],item[1])) in [foo(file) for file in ignore]]);
 
 for root,fname in fnames:
     targetdir = foo(os.path.join(os.environ.get("HOME"),root))
     if not os.path.exists(targetdir):
         os.makedirs(targetdir);
-        
+
     target = foo(os.path.join(targetdir,fname));
     base   = foo(os.path.join(root,fname));
     try:
@@ -34,7 +33,7 @@ for root,fname in fnames:
                     raise Exception;
         os.symlink(base,target);
         print("make symbolic link from %s to %s"%
-              (base,target))
+                (base,target))
     except:
         pass;
     else:
@@ -46,4 +45,8 @@ scriptname=os.path.join(os.getenv("HOME"),"bin","add-dotfiles-repos");
 with open(scriptname, "w") as of:
     of.write(open("add-dotfiles-repos").read().format(cwd=os.getcwd()));
 os.chmod(scriptname,
-         S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IROTH);
+        S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IROTH);
+
+for root,dirs,fnames in os.walk("init.d"):
+    for fname in fnames:
+        os.system(root+"/"+fname)
