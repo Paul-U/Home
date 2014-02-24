1d5535419e11b78d50f186c8672873498d754fe3
Paul-U@Vaio <kouzukekouryu@gmail.com>
2014-02-24 11:12:31 +0900

    Emacs : パッケージを一部整理

---
+++ .emacs.d/init.d/20-helm.el
@@ -0,0 +1,7 @@
+(require 'recentf)
+
+
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-for-files)
+(global-set-key (kbd "C-x M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
